033ff52903c31dee9c9bef71c298681ea2b64f5b
Quantumj1 <126617989+Quantumj1@users.noreply.github.com>
2025-11-05 22:14:36 +0000

    Create Footer.jsx

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#333',
+      color: 'white',
+      textAlign: 'center',
+      padding: '10px',
+      position: 'fixed',
+      bottom: '0',
+      width: '100%'
+    }}>
+      <p>&copy; 2023 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
