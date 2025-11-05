8bd328b79c1102f3d4a1e72b45d7759ab84f9f44
Quantumj1 <126617989+Quantumj1@users.noreply.github.com>
2025-11-05 21:20:54 +0000

    Update UserProfile.jsx

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 const UserProfile = (props) => {
    return (
      <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-       <h2>{props.name}</h2>
+       <h2><span style={{color: 'blue'}}>{props.name}</span></h2>
        <p>Age: {props.age}</p>
        <p>Bio: {props.bio}</p>
      </div>
