d6f075a062eff8dc3e9c73e241908e308cc40bed
Quantumj1 <126617989+Quantumj1@users.noreply.github.com>
2025-11-26 11:01:52 +0000

    Update UserProfile.jsx
    sm:w-24", "sm:h-24", "md:w-36", "md:h-36"
    contains the above for responsive image sizing on mobile phones or tablets

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
   return (
     <div class="user-profile bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg md:p-8 sm:p-4 md:max-w-sm">
-      <img src="https://via.placeholder.com/150" alt=" " className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto"/>
+      <img src="https://via.placeholder.com/150" alt=" " className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"/>
       <h1 class="text-lg sm:text-xl text-blue-800 my-4  md:text-xl">John Doe</h1>
       <p class="text-gray-600 text-sm sm:text-base ">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
